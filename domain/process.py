763c275befb5ced8f622fd315c46f6ba10351c0b
AlexDsJz <cduenasj1700@alumno.ipn.mx>
2022-11-30 22:52:48 -0600

    Update

--- domain/process.py
+++ domain/process.py
@@ -3,7 +3,10 @@ from django.shortcuts import get_object_or_404
 from domain.algorithm import get_minimum_value
 
 def create_process(string_triangle, user_id): 
+    initial_i = 0
+    initial_j = 0
     user = get_object_or_404(User, pk = user_id)
+    
     list = string_triangle.split(';')
     triangle = []
     try:
@@ -15,7 +18,7 @@ def create_process(string_triangle, user_id):
     except:
         print("Ocurrio un error")
         
-    minimum_sum = get_minimum_value(triangle, 0 , 0)
+    minimum_sum = get_minimum_value(triangle, initial_i, initial_j)
     
     process = Process.objects.create(input = triangle, output = minimum_sum, user = user)
     process.save()
